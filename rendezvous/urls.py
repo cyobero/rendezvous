184d8744e4931aacf18bd9a32a14b2578db90288
czar <cyobero@gmail.com>
2020-04-20 13:14:25 -0700

    added login view template

--- rendezvous/urls.py
+++ rendezvous/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rendezvous.views import index_view
+from login.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
+    path('login/', login_view, name='login'),
 ]
