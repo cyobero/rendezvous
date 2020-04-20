41f72f80820bf0895b3b3e05fedfac311d71c338
czar <cyobero@gmail.com>
2020-04-20 12:59:39 -0700

    created schedule.html

--- rendezvous/urls.py
+++ rendezvous/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index_view
+from rendezvous.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name='index')
+    path('', index_view, name='index'),
 ]
