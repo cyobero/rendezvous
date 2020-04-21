f228ae102dbdf6b2b7a636fd56a2f970dfbf2c62
czar <cyobero@gmail.com>
2020-04-21 07:27:39 -0700

    added  model

--- login/models.py
+++ login/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 PROFILE_TYPE_CHOICES = (
-    ('B', 'BUYER'),
-    ('D', 'DRIVER'),
+    ('B', 'BOOKER'),
+    ('A', 'APPOINTEE'),
 )
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_type = models.CharField(max_length=1, choices=PROFILE_TYPE_CHOICES)
+    birthdate = models.DateField(null=True, blank=True)
     image = models.ImageField(blank=True, null=True, upload_to='images/')
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
--- registration/views.py
+++ registration/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect, reverse
+
+from registration.forms import RegisterForm
 
 # Create your views here.
 def register_view(request):
-    return render(request, 'registration/register.html')
+    errors = []
+    form = RegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        user.refresh_from_db()  # load profile instance created by signal
+        user.user_profile.birthdate = form.cleaned_data.get('birthdate')
+        user.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(request, user)
+        return redirect(reverse('success'))
+    else:
+        return render(request, 'registration/register.html', {'form': form})
