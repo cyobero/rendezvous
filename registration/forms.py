df6401a44331c86c1d3e42504f72d508cacc704e
czar <cyobero@gmail.com>
2020-04-21 08:19:54 -0700

    created new registration form

---
+++ registration/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', ]
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'input100',
+                'placeholder': 'Username',
+            }),
+            'email': forms.TextInput(attrs={
+                'class': 'input100',
+                'placeholder': 'Email',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'input100',
+                'placeholder': 'Password'
+            })
+        }
